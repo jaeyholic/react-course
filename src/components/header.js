252d7815d719278b1f0b36409e34af671c4af411
Jeff Landen <gabsco208309@gmail.com>
2019-07-06 21:23:31 +0000

    adding background to header when scrolled

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,47 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useState, useEffect } from "react"
 import "./header.css"
 
 //Images
 import Logo from "../images/logo-designcode.svg"
 
-const Header = ({ siteTitle }) => (
-  <div className="header">
-    <div className="headerGroup">
-      <Link to="/">
-        <img
-          src={require("../images/logo-designcode.svg")}
-          alt="Logo"
-          width="30"
-        />
-      </Link>
-      <Link to="/courses">Courses</Link>
-      <Link to="/downloads">Downloads</Link>
-      <Link to="/workships">Workshops</Link>
-      <Link to="/buy">
-        <button>Buy</button>
-      </Link>
+const Header = ({ siteTitle }) => {
+  const [hasScrolled, setHasScrolled] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll)
+  })
+
+  const handleScroll = () => {
+    const scrollTop = window.pageYOffset
+    if (scrollTop > 50) {
+      setHasScrolled(true)
+    } else {
+      setHasScrolled(false)
+    }
+  }
+
+  return (
+    <div className={hasScrolled ? "header headerScrolled" : "header"}>
+      <div className="headerGroup">
+        <Link to="/">
+          <img
+            src={require("../images/logo-designcode.svg")}
+            alt="Logo"
+            width="30"
+          />
+        </Link>
+        <Link to="/courses">Courses</Link>
+        <Link to="/downloads">Downloads</Link>
+        <Link to="/workships">Workshops</Link>
+        <Link to="/buy">
+          <button>Buy</button>
+        </Link>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
